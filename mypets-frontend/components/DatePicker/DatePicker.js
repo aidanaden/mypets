61c27ba119085b6e0913f0ef124cf55858445e36
aidan-wsl <aidan@DESKTOP-73NEEJC.localdomain>
2021-06-16 03:24:36 +0800

    removed datepicker-react module

--- mypets-frontend/components/DatePicker/DatePicker.js
+++ mypets-frontend/components/DatePicker/DatePicker.js
@@ -1,39 +1,39 @@
-import DateFnsUtils from '@date-io/date-fns'
-import { chakra } from '@chakra-ui/react'
-import React, {useReducer} from 'react'
-import {DateSingleInput} from '@datepicker-react/styled'
+// import DateFnsUtils from '@date-io/date-fns'
+// import { chakra } from '@chakra-ui/react'
+// import React, {useReducer} from 'react'
+// import {DateSingleInput} from '@datepicker-react/styled'
 
-const StyledDateSingleInput = chakra(DateSingleInput)
+// const StyledDateSingleInput = chakra(DateSingleInput)
 
-function DatePicker({ field, onChange }) {
+// function DatePicker({ field, onChange }) {
 
-    return (
-        <StyledDateSingleInput
-            rounded='lg'
-            onDateChange={onChange}
-            date={null}
-            showDatepicker={false}
-            {...field}
-            id='dob'
-        />
-        // <MuiPickersUtilsProvider utils={DateFnsUtils}>
-        //     <KeyboardDatePicker
-        //         disableToolbar
-        //         {...field}
-        //         variant='static'
-        //         format='dd/MM/yyyy'
-        //         margin='normal'
-        //         id='dob'
-        //         KeyboardButtonProps={{
-        //             'aria-label': 'change date',
-        //         }}
-        //         onChange={onChange}
-        //         style={{
-        //             color: '#ffc400'
-        //         }}
-        //     />
-        // </MuiPickersUtilsProvider>
-    )
-}
+//     return (
+//         // <StyledDateSingleInput
+//         //     rounded='lg'
+//         //     onDateChange={onChange}
+//         //     date={null}
+//         //     showDatepicker={false}
+//         //     {...field}
+//         //     id='dob'
+//         // />
+//         // <MuiPickersUtilsProvider utils={DateFnsUtils}>
+//         //     <KeyboardDatePicker
+//         //         disableToolbar
+//         //         {...field}
+//         //         variant='static'
+//         //         format='dd/MM/yyyy'
+//         //         margin='normal'
+//         //         id='dob'
+//         //         KeyboardButtonProps={{
+//         //             'aria-label': 'change date',
+//         //         }}
+//         //         onChange={onChange}
+//         //         style={{
+//         //             color: '#ffc400'
+//         //         }}
+//         //     />
+//         // </MuiPickersUtilsProvider>
+//     )
+// }
 
-export default DatePicker
+// export default DatePicker
