6f92705e972499901230100348ced5edbcfcac55
aidan-wsl <aidan@DESKTOP-73NEEJC.localdomain>
2021-06-18 05:06:05 +0800

    updated store banners

--- mypets-frontend/components/HomeBannerSwiper/HomeBannerSwiper.js
+++ mypets-frontend/components/HomeBannerSwiper/HomeBannerSwiper.js
@@ -3,7 +3,7 @@ import BannerSwiper from '../BannerSwiper/BannerSwiper'
 
 function HomeBannerSwiper({...props}) {
 
-    const homeBannerImgNames = ["macho_pawz_banner.jpg", "discount_banner.jpg", "discount_banner_2.jpg"]
+    const homeBannerImgNames = ["banner-a.jpg", "banner-b.jpg", "banner-c.jpg"]
     return (
         <Box maxW='1200px' boxShadow='2xl' rounded={40} {...props}>
           <BannerSwiper bannerImgNames={homeBannerImgNames}/>
