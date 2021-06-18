7ab2e1554b5aa2fb3d8712255f05008e81f2ba65
aidan-wsl <aidan@DESKTOP-73NEEJC.localdomain>
2021-06-18 17:28:25 +0800

    use chakra-ui image with lazy loading for merchant banners + optimized banner sizes

--- mypets-frontend/components/BannerSwiper/BannerSwiper.js
+++ mypets-frontend/components/BannerSwiper/BannerSwiper.js
@@ -1,7 +1,6 @@
 import {
-    Box
+    Image
 } from '@chakra-ui/react'
-import NextImage from 'next/image'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import SwiperCore, { Autoplay, Navigation } from 'swiper/core';
 SwiperCore.use([Autoplay, Navigation]);
@@ -18,9 +17,7 @@ function BannerSwiper({ bannerImgNames }) {
         {
             bannerImgNames.map((bannerImgName,i) => (
                 <SwiperSlide key={i}>
-                    <Box rounded={40} boxSize="100%" objectFit="cover">
-                        <NextImage src={`/${bannerImgName}`} alt="merchant banner" width='1200' height='411'/>
-                    </Box>
+                    <Image loading='lazy' rounded={40} boxSize="100%" objectFit="cover" src={`/${bannerImgName}`} alt="merchant banner"/>
                 </SwiperSlide>
             ))
         }
