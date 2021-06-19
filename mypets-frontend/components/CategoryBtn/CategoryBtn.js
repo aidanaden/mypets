915a6449e5d6b820029ccf25467806838b13c84f
aidan-wsl <aidan@DESKTOP-73NEEJC.localdomain>
2021-06-20 01:13:54 +0800

    set categorybtn box hover pointer + adjusted padding size

--- mypets-frontend/components/CategoryBtn/CategoryBtn.js
+++ mypets-frontend/components/CategoryBtn/CategoryBtn.js
@@ -8,12 +8,14 @@ function CategoryBtn({ cat, onClick }) {
     return (
         <Box
             w="100%"  
-            p={6}
+            py={4}
+            px={6}
             rounded="md" 
             bgColor="gray.100" 
             textColor="gray.600"
+            textAlign='center'
             fontSize="lg"
-            _hover={{ bgGradient: "linear(to-t, mypets.900, mypets.100)", textColor: "gray.100" }}
+            _hover={{ bgGradient: "linear(to-t, mypets.900, mypets.100)", textColor: "gray.100", cursor: "pointer" }}
             _active={{ transform: "scale(0.95)"}}
             onClick={() => onClick(cat)}
         >
