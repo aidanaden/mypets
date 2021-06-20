bac176aa9e65ed91bc17f743409fc8042d21e6f1
aidan-wsl <aidan@DESKTOP-73NEEJC.localdomain>
2021-06-20 17:38:28 +0800

    100% w stack for announcement bar

--- mypets-frontend/components/AnnouncementBanner/AnnouncementBanner.js
+++ mypets-frontend/components/AnnouncementBanner/AnnouncementBanner.js
@@ -15,12 +15,11 @@ function AnnouncementBanner() {
                 py={3}
                 px={3}
                 bgGradient="linear(to-t, mypets.900, mypets.100)"
+                w='100%'
             >
-                {/* <HStack spacing="3"> */}
                 <Text fontWeight="sm" marginEnd="2" textColor='gray.800'>
                     We are only delivering to the EAST of SG for now 😢
                 </Text>
-                {/* </HStack> */}
             </Stack>
         </Box>
     )
