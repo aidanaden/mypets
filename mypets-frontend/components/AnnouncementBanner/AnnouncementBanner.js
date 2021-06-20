e85001eae9a59d766ad6463a52afbca02617f4fe
aidan-wsl <aidan@DESKTOP-73NEEJC.localdomain>
2021-06-20 17:42:00 +0800

    set base width to 100% for stack for announcement bar

--- mypets-frontend/components/AnnouncementBanner/AnnouncementBanner.js
+++ mypets-frontend/components/AnnouncementBanner/AnnouncementBanner.js
@@ -7,7 +7,7 @@ import {
 
 function AnnouncementBanner() {
     return (
-        <Box w='100%'>
+        <Box w={{ base: '100%' }}>
             <Stack
                 direction='row'
                 justifyContent="center"
@@ -15,7 +15,7 @@ function AnnouncementBanner() {
                 py={3}
                 px={3}
                 bgGradient="linear(to-t, mypets.900, mypets.100)"
-                w='100%'
+                w={{ base: '100%' }}
             >
                 <Text fontWeight="sm" marginEnd="2" textColor='gray.800'>
                     We are only delivering to the EAST of SG for now 😢
