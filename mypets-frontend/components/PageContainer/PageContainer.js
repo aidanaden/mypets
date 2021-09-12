603660d9277783a268e1b620cd937d6d871ab1ce
aidan-wsl <aidan@DESKTOP-73NEEJC.localdomain>
2021-09-12 22:48:48 +0800

    remove mb from PageContainer

--- mypets-frontend/components/PageContainer/PageContainer.js
+++ mypets-frontend/components/PageContainer/PageContainer.js
@@ -4,7 +4,6 @@ function PageContainer({ children, ...props }) {
     return (
         <Container
             maxW={{ lg: "1200px" }}
-            mb={4}
             px={{ base: 4 }}
             py={{ base: 8, md: 16 }}
             {...props}
