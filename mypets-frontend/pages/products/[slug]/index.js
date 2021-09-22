c933a7f63e62c5228b27caa504f55a557a6e9f41
aidan-wsl <aidan@DESKTOP-73NEEJC.localdomain>
2021-09-22 21:32:39 +0800

    fetch category products in getStaticProps

--- mypets-frontend/pages/products/[slug]/index.js
+++ mypets-frontend/pages/products/[slug]/index.js
@@ -61,36 +61,31 @@ export default function Product({ product, otherProducts }) {
 }
 
 export async function getStaticProps({ params: { slug, otherProducts } }) {
-    console.log('other products: ', otherProducts)
     const product_res = await fetch(`${API_PRODUCTS_URL}?slug=${slug}`)
     const product = await product_res.json()
+    const category_res = await fetch(`${API_PRODUCTS_URL}?category=${product.category.id}`)
+    const categoryProducts = await category_res.json()
+
+    console.log('other products: ', categoryProducts)
 
     // Return as props
     return {
         props: {
             product: product[0],
-            otherProducts: otherProducts.filter(otherProduct => otherProduct !== product[0])
+            otherProducts: categoryProducts
         }
     }
-}
-
-function getSuggestedProducts(product, otherProducts) {
-    console.log('page product: ', product)
-    console.log('other products: ', otherProducts)
-    return otherProducts.filter(otherProduct => otherProduct.category.id == product.category.id)
 }
 
 export async function getStaticPaths() {
     // retrieve all possible paths
     const product_res = await fetch(`${API_PRODUCTS_URL}`)
     const products = await product_res.json()
-    console.log('get static path products: ', products)
     // return to NextJS context
     return {
         paths: products.map(product => ({
             params: { 
-                slug: String(product.slug),
-                otherProducts: getSuggestedProducts(product, products)
+                slug: String(product.slug)
             }
         })),
 
