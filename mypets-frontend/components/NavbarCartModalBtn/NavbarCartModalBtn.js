7be739a1f0a81068eeaa132b069f1a16dd442668
aidan-wsl <aidan@DESKTOP-73NEEJC.localdomain>
2021-09-23 22:42:26 +0800

    add missing imports

--- mypets-frontend/components/NavbarCartModalBtn/NavbarCartModalBtn.js
+++ mypets-frontend/components/NavbarCartModalBtn/NavbarCartModalBtn.js
@@ -9,7 +9,8 @@ import {
     ModalHeader,
     ModalBody,
     ModalCloseButton,
-    Box
+    Box,
+    Center
 } from '@chakra-ui/react'
 import lodash from 'lodash'
 import { loadStripe } from '@stripe/stripe-js'
