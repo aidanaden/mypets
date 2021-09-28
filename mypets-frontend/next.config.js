9707377914a534e660145694d42157415b3dc6c4
aidan-wsl <aidan@DESKTOP-73NEEJC.localdomain>
2021-09-29 02:46:49 +0800

    testing with reactMarkdwon slowly 2

--- mypets-frontend/next.config.js
+++ mypets-frontend/next.config.js
@@ -1,12 +1,13 @@
-// const withTM = require('next-transpile-modules')(['react-markdown']);
+const withTM = require('next-transpile-modules')(['react-markdown']);
 
-// module.exports = withTM({
-//     images: {
-//       domains: ['localhost', 'mypets-images.s3.ap-southeast-1.amazonaws.com']
-//     }
-//   })
-module.exports = {
-  images: {
-    domains: ['localhost', 'mypets-images.s3.ap-southeast-1.amazonaws.com']
-  }
-}
+module.exports = withTM({
+    images: {
+      domains: ['localhost', 'mypets-images.s3.ap-southeast-1.amazonaws.com']
+    },
+    webpack5: false
+  })
+// module.exports = {
+//   images: {
+//     domains: ['localhost', 'mypets-images.s3.ap-southeast-1.amazonaws.com']
+//   }
+// }
