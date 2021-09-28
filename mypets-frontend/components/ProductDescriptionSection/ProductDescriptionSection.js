3a4a76cfb038c11f59e2716d00022dba1371ea01
aidan-wsl <aidan@DESKTOP-73NEEJC.localdomain>
2021-09-29 01:22:44 +0800

    add product nutrient analysis to product page

--- mypets-frontend/components/ProductDescriptionSection/ProductDescriptionSection.js
+++ mypets-frontend/components/ProductDescriptionSection/ProductDescriptionSection.js
@@ -1,9 +1,81 @@
 import React from 'react'
-import { Box, Heading, List, Text } from '@chakra-ui/react'
+import {
+    Box,
+    Heading,
+    List,
+    Text,
+    Stack
+} from '@chakra-ui/react'
 import ProductDescriptionListItem from '../ProductDescriptionListItem/ProductDescriptionListItem'
 
-function ProductDescriptionSection({ product }) {
+const ProductDescription = ({ descriptions }) => {
+    return (
+        <Box>
+            <Heading
+                as="h3"
+                fontSize="2xl"
+                textAlign="center"
+            >
+                Description
+            </Heading>
+            <List
+                spacing={3}
+                textAlign='center'
+            >
+                {descriptions.map((description, i) => (
+                    <ProductDescriptionListItem
+                        key={i}
+                        description={description}
+                    />
+                ))}
+            </List>
+        </Box>
+    )
+}
 
+const ProductIngredient = ({ ingredients }) => {
+    return (
+        <Box>
+            <Heading
+                as="h3"
+                fontSize="2xl"
+                textAlign="center"
+            >
+                Ingredients
+                </Heading>
+            <Text>
+                {ingredients}
+            </Text>
+        </Box>
+    )
+}
+
+const ProductNutrients = ({ nutrients }) => {
+    // nutrients = ["chicken - 30%"]
+    return (
+        <Center
+            p={{ base: 4 }}
+        >
+            {nutrients.map((nutrient, i) => (
+                <Stack
+                    direction='row'
+                    // spacing={{ base: 1 }}
+                    justify='space-between'
+                >
+                    <Text>
+                        {nutrient.split('-')[0]}
+                    </Text>
+                    <Spacer />
+                    <Text>
+                        {nutrient.split('-')[1]}
+                    </Text>
+                </Stack>
+            ))}
+        </Center>
+    )
+}
+
+export default function ProductDescriptionSection({ product }) {
     // const descriptions = [
     //     'Grain-free/soy-free/wheat-free',
     //     'Yucca schidigera extract which reduces odors associated with waste',
@@ -13,21 +85,17 @@ function ProductDescriptionSection({ product }) {
     //     'Omega-3 & 6 fatty acids  that promotes healthy skin and shiny coat'
     // ]
     const descriptions = String(product.content).replace(/[0-9.]/g, "").split(/[\r\n]+/)
+    const nutrients = product.nutrional_analysis.split(/[\r\n]+/)
 
     return (
-        <Box py={8} px={8}>
-            <Heading as="h3" fontSize="2xl" textAlign="center" mb={6}>Description</Heading>
-            <List spacing={3} textAlign='center'> 
-                {descriptions.map((description, i) => (
-                    <ProductDescriptionListItem key={i} description={description}/>   
-                ))}
-            </List>
-            <Heading as="h3" fontSize="2xl" textAlign="center" mb={6} mt={24}>Ingredients</Heading>
-            <Text>
-                {product.ingredients}
-            </Text>
-        </Box>
+        <Stack
+            p={8}
+            direction='column'
+            spacing={{ base: 6 }}
+        >
+            <ProductDescription descriptions={descriptions} />
+            <ProductIngredient ingredients={product.ingredients} />
+            <ProductNutrients nutrients={nutrients} />
+        </Stack>
     )
 }
-
-export default ProductDescriptionSection
