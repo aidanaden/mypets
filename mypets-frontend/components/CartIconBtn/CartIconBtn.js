434e6e4ce24b13ff1b46294dfd5d808fa00099ee
aidan-wsl <aidan@DESKTOP-73NEEJC.localdomain>
2021-09-28 19:15:27 +0800

    test adding num order products to cart icon  use circle 5

--- mypets-frontend/components/CartIconBtn/CartIconBtn.js
+++ mypets-frontend/components/CartIconBtn/CartIconBtn.js
@@ -14,21 +14,20 @@ export default function CartIconBtn({ cartNumOrderProducts, ...props }) {
             icon={
                 <>
                     <Icon as={FaShoppingCart}
-                        boxSize={{ base: 8 }}
+                        boxSize={{ base: 6 }}
                     />
                     {cartNumOrderProducts > 0 &&
                     <Circle
                         pos="absolute"
                         top="-1px"
                         right="-1px"
-                        p={{ base: 1 }}
+                        p={{ base: 2 }}
                         fontSize="xs"
                         fontWeight="bold"
                         lineHeight="none"
                         color="white"
                         transform="translate(50%,-50%)"
                         bg="mypets.400"
-                        rounded="full"
                     >
                         {cartNumOrderProducts}
                     </Circle>}
