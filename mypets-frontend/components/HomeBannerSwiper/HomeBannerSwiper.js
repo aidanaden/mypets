d4d47453de2768dbdb5ce3f986c7e17694d739f6
aidan-wsl <aidan@DESKTOP-73NEEJC.localdomain>
2021-10-18 22:38:24 +0800

    swap to swiper 5

--- mypets-frontend/components/HomeBannerSwiper/HomeBannerSwiper.js
+++ mypets-frontend/components/HomeBannerSwiper/HomeBannerSwiper.js
@@ -2,11 +2,13 @@ import { Box } from '@chakra-ui/react'
 import BannerSwiper from '../BannerSwiper/BannerSwiper'
 
 export default function HomeBannerSwiper({rounded, ...props}) {
-    const homeBannerImgNames = ["desktop.jpeg", "banner-c.jpg"]
-    const mobileBannerImgNames=["mobile.jpeg", "banner-c.jpg"]
+    const homeBannerImgNames = ["desktop.jpeg"]
+    const mobileBannerImgNames=["mobile.jpeg"]
 
     return (
         <Box
+          mt={{ base: 4, md: 8 }}
+          mb={{ base: 8, md: 16 }}
           w='100%'
           boxShadow='2xl'
           rounded={rounded}
