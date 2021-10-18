0399f12d8e790b90fb5bc2df1dd71fb4a98a451d
aidan-wsl <aidan@DESKTOP-73NEEJC.localdomain>
2021-10-18 20:02:53 +0800

    try remake v13

--- mypets-frontend/next.config.js
+++ mypets-frontend/next.config.js
@@ -1,4 +1,4 @@
-const withTM = require('next-transpile-modules')(['react-markdown', 'remark-gfm', 'rehype-raw']);
+const withTM = require('next-transpile-modules')(['react-markdown', 'remark-gfm']);
 
 module.exports = withTM({
     images: {
