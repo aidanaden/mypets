443f7a0ef4609d2156b9edf8cad8fb745f8cb5d9
aidan-wsl <aidan@DESKTOP-73NEEJC.localdomain>
2021-10-30 20:52:32 +0800

    test orderProduct btn formatting 8

--- mypets-frontend/components/OrderProductCard/OrderProductCard.js
+++ mypets-frontend/components/OrderProductCard/OrderProductCard.js
@@ -136,11 +136,11 @@ export default function OrderProductCard({ order_products }) {
                     </Table>
                     <Stack
                         direction={{ base: 'column', md: 'row' }}
-                        // spacing={{ base: 2, md: 4 }}
+                        spacing={{ base: 2, md: 4 }}
                         textAlign='center'
                         mt={{ base: 8 }}
-                        // justifyContent='center'
-                        // alignContent='center'
+                        justifyContent='center'
+                        alignContent='center'
                         bg='green.100'
                     >
                         <OrderProductReviewModalBtn order_product={order_products[0]}/>
