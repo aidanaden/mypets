6fcb708132871d9d7a910508d894a8738be3c526
aidan-wsl <aidan@DESKTOP-73NEEJC.localdomain>
2021-10-31 23:07:08 +0800

    fixing sold out badge, privacy policy format again 4

--- mypets-frontend/components/MerchantBadge/MerchantBadge.js
+++ mypets-frontend/components/MerchantBadge/MerchantBadge.js
@@ -7,7 +7,7 @@ function MerchantBadge({ merchantName, ...props }) {
             rounded='full'
             px="2"
             maxW='100%'
-            fontSize={{ base: 'xs', md: 'sm' }}
+            fontSize={{ base: 'xs', lg: 'sm' }}
             {...props}
         >
             {merchantName}
