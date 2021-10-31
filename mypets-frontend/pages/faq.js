9ef4a33ea56511759ad0ce52c42d012db48e19a3
aidan-wsl <aidan@DESKTOP-73NEEJC.localdomain>
2021-11-01 01:13:09 +0800

    format faq, privacy test 2

--- mypets-frontend/pages/faq.js
+++ mypets-frontend/pages/faq.js
@@ -8,6 +8,8 @@ import Sidebar from '../components/Sidebar/Sidebar'
 import Footer from '../components/Footer/Footer'
 import { API_CATEGORIES_URL } from '../utils/urls'
 import SectionHeader from '../components/SectionHeader/SectionHeader'
+import SectionSubHeader from '../components/SectionSubHeader/SectionSubHeader'
+import ParagraphSection from '../components/ParagraphSection/ParagraphSection'
 
 export default function faq({ categories }) {
     const markdownText = `
