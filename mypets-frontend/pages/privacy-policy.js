6c36e6c893da6b8f40e295596e843d461c0d45f6
aidan-wsl <aidan@DESKTOP-73NEEJC.localdomain>
2021-10-31 22:39:18 +0800

    add product sold out badge 10

--- mypets-frontend/pages/privacy-policy.js
+++ mypets-frontend/pages/privacy-policy.js
@@ -12,20 +12,19 @@ import SectionHeader from '../components/SectionHeader/SectionHeader'
 import customTheme from '../styles/theme'
 
 export default function privacy({ categories }) {
-    const markdownText = `
-    # MyPets Privacy Policy
+    const markdownText = `# MyPets Privacy Policy
     
-    Last Updated: 19/10/21
+Last Updated: 19/10/21
     
-    This Privacy Policy describes our policies and procedures on how we collect, use and disclose the information provided when you use our site.
+This Privacy Policy describes our policies and procedures on how we collect, use and disclose the information provided when you use our site.
     
-    MyPets uses your personal data to provide and improve our service &amp; platform. By using our platform, you agree to the collection and use of information in accordance with this Privacy Policy. This Privacy Policy has been created with the help of the [Privacy Policy Generator](https://www.freeprivacypolicy.com/free-privacy-policy-generator/).
+MyPets uses your personal data to provide and improve our service &amp; platform. By using our platform, you agree to the collection and use of information in accordance with this Privacy Policy. This Privacy Policy has been created with the help of the [Privacy Policy Generator](https://www.freeprivacypolicy.com/free-privacy-policy-generator/).
     
-    The term MyPets/MyPets Singapore refers to the same entity.
+The term MyPets/MyPets Singapore refers to the same entity.
+
+## Definitions
     
-    ## Definitions
-    
-    For the purposes of this Privacy Policy:
+For the purposes of this Privacy Policy:
     
     - Account means a unique account created in order to use our site.
     - Company (referred to as either &quot;the Company&quot;, &quot;We&quot;, &quot;Us&quot; or &quot;Our&quot; in this Agreement) refers to MyPets Singapore.
@@ -39,13 +38,13 @@ export default function privacy({ categories }) {
     - Website refers to MyPets Singapore, accessible from [www.mypets.sg](https://cdpn.io/cp/internal/boomboom/mypets.sg)
     - You refer to the account owner or representatives on behalf of the account owner that is accessing our service
     
-    ## Data Collection &amp; Usage
+## Data Collection &amp; Usage
     
-    ### Types Of Data Collected
+### Types Of Data Collected
     
-    ### Personal Data
+### Personal Data
     
-    When using our service, we may ask you to provide us with certain personally identifiable information that can be used to contact or identify you. (&quot;Personal Data&quot;) may include, but is not limited to:
+When using our service, we may ask you to provide us with certain personally identifiable information that can be used to contact or identify you. (&quot;Personal Data&quot;) may include, but is not limited to:
     
     - Email address
     - First name and last name
@@ -53,30 +52,30 @@ export default function privacy({ categories }) {
     - Address, State, Province, ZIP/Postal code, City
     - Cookies &amp; Usage Data
     
-    ### Usage Data
+### Usage Data
     
-    Usage Data is collected automatically when you use our service. We may take information regarding your device&#39;s Internet Protocol address (e.g. IP address), browser type, browser version, the pages of our service that you visit, the time and date of your visit, the time spent on those pages, unique device identifiers and other diagnostic data.
+Usage Data is collected automatically when you use our service. We may take information regarding your device&#39;s Internet Protocol address (e.g. IP address), browser type, browser version, the pages of our service that you visit, the time and date of your visit, the time spent on those pages, unique device identifiers and other diagnostic data.
     
-    Likewise, when accessing our service through a mobile device, we may also collect information regarding, the type of mobile device you use, your mobile device unique ID, the IP address of your mobile device, it&#39;s mobile operating system, the type of mobile Internet browser that you use, unique device identifiers and other diagnostic data.
+Likewise, when accessing our service through a mobile device, we may also collect information regarding, the type of mobile device you use, your mobile device unique ID, the IP address of your mobile device, it&#39;s mobile operating system, the type of mobile Internet browser that you use, unique device identifiers and other diagnostic data.
     
-    ### Information From Third-Party Social Media Services
+### Information From Third-Party Social Media Services
     
-    MyPets allows you to create an account and log in to use our service through the following third-party social media services:
+MyPets allows you to create an account and log in to use our service through the following third-party social media services:
     
-    - Google
-    - Facebook
+- Google
+- Facebook
     
-    If you decide to register through a third-party social media service, we may collect personal data that is already associated with them.
+If you decide to register through a third-party social media service, we may collect personal data that is already associated with them.
     
-    By signing up with either of the above third-party social media service providers, you are aware &amp; have granted us the permission to use, share, and store it in a manner consistent with this Privacy Policy.
+By signing up with either of the above third-party social media service providers, you are aware &amp; have granted us the permission to use, share, and store it in a manner consistent with this Privacy Policy.
     
-    ### Collection of data (&quot;Cookies&quot;)
+### Collection of data (&quot;Cookies&quot;)
     
-    We use Cookies to track the activity on our service and store certain information.
+We use Cookies to track the activity on our service and store certain information.
     
-    Cookies are files with a small amount of data which may include an anonymous unique identifier. Cookies are sent to your browser from a website and stored on your device. Tracking technologies also used are beacons, tags, and scripts to collect and track information and to improve and analyze our Service.
+Cookies are files with a small amount of data which may include an anonymous unique identifier. Cookies are sent to your browser from a website and stored on your device. Tracking technologies also used are beacons, tags, and scripts to collect and track information and to improve and analyze our Service.
     
-    We use cookies for our service for the following purposes:
+We use cookies for our service for the following purposes:
     
     - Enabling certain features and functions on our websites, e.g. remembering your details, browsing and other services preferences
     - Building up a profile of how you and other users use the website
@@ -84,14 +83,11 @@ export default function privacy({ categories }) {
     - Administering services to you and to advertisers
     - Establishing usage statistics
     
-    You can choose to opt out all cookies or indicate when a cookie is being sent. However, there might be a case where you may not be able to use some portions of our service.
+You can choose to opt out all cookies or indicate when a cookie is being sent. However, there might be a case where you may not be able to use some portions of our service.
     
-    ##
+## Personal Data Use
     
-    
-    ## Personal Data Use
-    
-    We may use your Personal Data for the following purposes:
+We may use your Personal Data for the following purposes:
     
     - Provide and maintain our service.
     - Provide access to current functions &amp; inform you about new functions that can benefit you as a registered user.
@@ -103,29 +99,29 @@ export default function privacy({ categories }) {
     - Notify you about changes to our service and regular maintenance
     - Improve our business operations by with the help of data analytics
     
-    We may share your personal information:
+We may share your personal information:
     
-    - With business partners to offer you certain products, services or promotions.
+- With business partners to offer you certain products, services or promotions.
     
-    ## Retention Of Your Personal Data
+## Retention Of Your Personal Data
     
-    The Company will retain your personal data only for as long as is necessary for the purposes set out in this Privacy Policy. We will retain and use your personal data to the extent necessary to comply with our legal obligations (for example, if we are required to retain your data to comply with applicable laws), resolve disputes, and enforce our legal agreements and policies.
+The Company will retain your personal data only for as long as is necessary for the purposes set out in this Privacy Policy. We will retain and use your personal data to the extent necessary to comply with our legal obligations (for example, if we are required to retain your data to comply with applicable laws), resolve disputes, and enforce our legal agreements and policies.
     
-    We will also retain usage data for internal analysis purposes. Usage data is generally retained for a shorter period of time, except when this data is used to strengthen the security or to improve the functionality of our service, or we are legally obligated to retain this data for longer time periods.
+We will also retain usage data for internal analysis purposes. Usage data is generally retained for a shorter period of time, except when this data is used to strengthen the security or to improve the functionality of our service, or we are legally obligated to retain this data for longer time periods.
     
-    ## Data Transfer
+## Data Transfer
     
-    Your information, including Personal Data, may be transferred to — and maintained on — computers located outside Your state, province, country or other governmental jurisdiction where the data protection laws may differ from those from Your jurisdiction.
+Your information, including Personal Data, may be transferred to — and maintained on — computers located outside Your state, province, country or other governmental jurisdiction where the data protection laws may differ from those from Your jurisdiction.
     
-    Your consent to this Privacy policy followed by submission of your personal information represents Your agreement to that transfer.
+Your consent to this Privacy policy followed by submission of your personal information represents Your agreement to that transfer.
     
-    We will take all steps necessary to ensure that your data is treated securely and in accordance with this Privacy Policy and no transfer of your Personal Data will take place to an organization or a country unless there are adequate controls in place including the security of your data and other personal information.
+We will take all steps necessary to ensure that your data is treated securely and in accordance with this Privacy Policy and no transfer of your Personal Data will take place to an organization or a country unless there are adequate controls in place including the security of your data and other personal information.
     
-    ## Disclosure Of Data
+## Disclosure Of Data
     
-    ### Regulations
+### Regulations
     
-    MyPets may disclose your Personal Data in the good faith belief that such action is necessary to:
+MyPets may disclose your Personal Data in the good faith belief that such action is necessary to:
     
     - Comply with a legal obligation
     - Protect and defend the rights or property of the Company
@@ -133,41 +129,41 @@ export default function privacy({ categories }) {
     - Protect the personal safety of Users of the Service or the public
     - Protect against legal liability
     
-    ## Security of your personal data
+## Security of your personal data
     
-    The security of your Personal Data is important to Us, but remember that no method of transmission over the Internet, or method of electronic storage is 100% secure. While We strive to use commercially acceptable means to protect Your Personal Data, We cannot guarantee its absolute security.
+The security of your Personal Data is important to Us, but remember that no method of transmission over the Internet, or method of electronic storage is 100% secure. While We strive to use commercially acceptable means to protect Your Personal Data, We cannot guarantee its absolute security.
     
-    ## Children&#39;s Privacy
+## Children&#39;s Privacy
     
-    Our service does not address anyone under the age of 18. We do not knowingly collect personally identifiable information from anyone under the age of 18. If You are a parent or guardian, and you are aware that your child has provided us with personal data, please inform us. If We become aware that we have collected personal data from anyone under the age of 18 without verification of parental consent, We will take the necessary steps to remove that information from our servers.
+Our service does not address anyone under the age of 18. We do not knowingly collect personally identifiable information from anyone under the age of 18. If You are a parent or guardian, and you are aware that your child has provided us with personal data, please inform us. If We become aware that we have collected personal data from anyone under the age of 18 without verification of parental consent, We will take the necessary steps to remove that information from our servers.
     
-    ## Links To Other Websites
+## Links To Other Websites
     
-    Our Service may contain links to other websites that are not operated by us. If you click on a third party link, You will be directed to that third party&#39;s site. We strongly advise you to review the Privacy Policy of every site you visit.
+Our Service may contain links to other websites that are not operated by us. If you click on a third party link, You will be directed to that third party&#39;s site. We strongly advise you to review the Privacy Policy of every site you visit.
     
-    We have no control over and assume no responsibility for the content, privacy policies or practices of any third party sites or services.
+We have no control over and assume no responsibility for the content, privacy policies or practices of any third party sites or services.
     
-    ## Changes To This Privacy Policy
+## Changes To This Privacy Policy
     
-    We may update our Privacy Policy from time to time. We will notify you of any changes by posting the new Privacy Policy on this page.
+We may update our Privacy Policy from time to time. We will notify you of any changes by posting the new Privacy Policy on this page.
     
-    We will let You know via email and/or a prominent notice on Our Service, prior to the change becoming effective, and update the &quot;Last updated&quot; date at the top of this Privacy Policy.
+We will let You know via email and/or a prominent notice on Our Service, prior to the change becoming effective, and update the &quot;Last updated&quot; date at the top of this Privacy Policy.
     
-    You are advised to review this Privacy Policy periodically for any changes. Changes to this Privacy Policy are effective when they are posted on this page.
+You are advised to review this Privacy Policy periodically for any changes. Changes to this Privacy Policy are effective when they are posted on this page.
     
-    ## Contact Us
+## Contact Us
     
-    If you have any questions about this Privacy Policy, you can contact us:
+If you have any questions about this Privacy Policy, you can contact us:
     
-    - By email: contactmypetssg@gmail.com
-    - By visiting this page on our website: [www.mypets.sg](https://cdpn.io/cp/internal/boomboom/www.mypets.sg)
+- By email: contactmypetssg@gmail.com
+- By visiting this page on our website: [www.mypets.sg](https://cdpn.io/cp/internal/boomboom/www.mypets.sg)
     
-    ## Delivery
+## Delivery
     
-    If you have any questions about this Privacy Policy, you can contact us:
+If you have any questions about this Privacy Policy, you can contact us:
     
-    - By email: contactmypetssg@gmail.com
-    - By visiting this page on our website: [www.mypets.sg](https://cdpn.io/cp/internal/boomboom/www.mypets.sg)`
+- By email: contactmypetssg@gmail.com
+- By visiting this page on our website: [www.mypets.sg](https://cdpn.io/cp/internal/boomboom/www.mypets.sg)`
 
     return (
         <Box>
