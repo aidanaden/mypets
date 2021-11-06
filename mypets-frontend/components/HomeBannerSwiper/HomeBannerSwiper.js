aa08fb7a9366bfb9732c9f3239a955511dc693a9
aidan-wsl <aidan@DESKTOP-73NEEJC.localdomain>
2021-11-06 16:25:29 +0800

    added new live banner

--- mypets-frontend/components/HomeBannerSwiper/HomeBannerSwiper.js
+++ mypets-frontend/components/HomeBannerSwiper/HomeBannerSwiper.js
@@ -2,8 +2,8 @@ import { Box } from '@chakra-ui/react'
 import BannerSwiper from '../BannerSwiper/BannerSwiper'
 
 export default function HomeBannerSwiper({ rounded={ base: 20, md: 40 }, ...props }) {
-    const homeBannerImgNames = ["desktop-delivery.jpeg", "desktop-discount.jpeg", "desktop-locations.jpeg"]
-    const mobileBannerImgNames=["mobile-delivery.jpeg", "mobile-discount.jpeg", "mobile-locations.jpeg"]
+    const homeBannerImgNames = ["desktop-delivery.jpeg", "desktop-discount.jpeg", "desktop-locations.jpeg", "desktop-live.jpeg"]
+    const mobileBannerImgNames=["mobile-delivery.jpeg", "mobile-discount.jpeg", "mobile-locations.jpeg", "mobile-live.jpeg"]
 
     return (
         <Box
