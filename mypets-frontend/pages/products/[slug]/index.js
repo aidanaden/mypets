663e6fd0b1584a2e4c1bb11bf8d34eebd0980979
aidan-wsl <aidan@DESKTOP-73NEEJC.localdomain>
2021-12-31 00:44:24 +0800

    add announcementbar text from products page

--- mypets-frontend/pages/products/[slug]/index.js
+++ mypets-frontend/pages/products/[slug]/index.js
@@ -13,13 +13,13 @@ import ProductDetailSection from "../../../components/ProductDetailSection/Produ
 import ProductDescriptionSection from "../../../components/ProductDescriptionSection/ProductDescriptionSection"
 import ProductReviewSection from "../../../components/ProductReviewSection/ProductReviewSection"
 import ProductList from '../../../components/ProductList/ProductList'
-import { API_PRODUCTS_URL } from '../../../utils/urls'
+import { API_HOME_URL, API_PRODUCTS_URL } from '../../../utils/urls'
 import PageContainer from '../../../components/PageContainer/PageContainer'
 
-export default function Product({ product, otherProducts }) {
+export default function Product({ bannerTitle, product, otherProducts }) {
     return (
         <Box>
-            <AnnouncementBanner />
+            <AnnouncementBanner text={bannerTitle} />
             <Sidebar />
             <PageContainer>
                 <BackBtn />
@@ -69,12 +69,15 @@ export async function getStaticProps({ params: { slug } }) {
     const product = await product_res.json()
     const category_res = await fetch(`${API_PRODUCTS_URL}?category=${product[0].category.id}`)
     const categoryProducts = await category_res.json()
+    const home_res = await fetch(`${API_HOME_URL}`)
+    const home_data = await home_res.json()
 
     // Return as props
     return {
         props: {
             product: product[0],
-            otherProducts: categoryProducts.filter(otherProduct => otherProduct.id != product[0].id)
+            otherProducts: categoryProducts.filter(otherProduct => otherProduct.id != product[0].id),
+            bannerTitle: home_data.banner_text
         },
         // Next.js will attempt to re-generate the page:
         // - When a request comes in
