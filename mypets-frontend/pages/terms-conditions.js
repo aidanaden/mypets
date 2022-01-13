47b25f5c356d243729d2a357011e6f6043a1f7f0
aidan-wsl <aidan@DESKTOP-73NEEJC.localdomain>
2022-01-13 18:11:39 +0800

    remove log from terms page

--- mypets-frontend/pages/terms-conditions.js
+++ mypets-frontend/pages/terms-conditions.js
@@ -13,7 +13,6 @@ import ParagraphSection from '../components/ParagraphSection/ParagraphSection'
 import SectionSubHeader from '../components/SectionSubHeader/SectionSubHeader'
 
 export default function terms({ categories, terms_data }) {
-    console.log('terms data: ', terms_data)
     return (
         <>
             <Head>
