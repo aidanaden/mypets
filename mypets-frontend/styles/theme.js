3180a640126085ad23c5e816bc85bd595a18a8a6
aidanaden <aidanaden@hotmail.com>
2022-03-06 19:19:53 +0800

    update prod frontend config to light mode

--- mypets-frontend/styles/theme.js
+++ mypets-frontend/styles/theme.js
@@ -1,52 +1,52 @@
-import { extendTheme } from "@chakra-ui/react"
-import { theme as chakraTheme } from "@chakra-ui/react"
+import { extendTheme } from "@chakra-ui/react";
+import { theme as chakraTheme } from "@chakra-ui/react";
 
 const fonts = {
-    ...chakraTheme.fonts,
-    // futura: "Futura",
-    // gothamrnd: "GothamRnd",
-    heading: `Futura`,
-    body: `Poppins`
-}
+  ...chakraTheme.fonts,
+  // futura: "Futura",
+  // gothamrnd: "GothamRnd",
+  heading: `Futura`,
+  body: `Poppins`,
+};
 
 const colors = {
-    ...chakraTheme.colors,
-    mypets: {
-        100: "#ffc400",
-        200: "#ffc400",
-        300: "#ffc400",
-        400: "#ffc400",
-        500: "#ffc400",
-        600: "#ffc400",
-        700: "#ffc400",
-        800: "#ffc400",
-        900: "#ffa000",
-    },
-    "mypets-green": {
-        100: "#34A853",
-        200: "#34A853",
-        300: "#34A853",
-        400: "#34A853",
-        500: "#34A853",
-        600: "#34A853",
-        700: "#34A853",
-        800: "#34A853",
-        900: "#34A853",
-    }
-}
+  ...chakraTheme.colors,
+  mypets: {
+    100: "#ffc400",
+    200: "#ffc400",
+    300: "#ffc400",
+    400: "#ffc400",
+    500: "#ffc400",
+    600: "#ffc400",
+    700: "#ffc400",
+    800: "#ffc400",
+    900: "#ffa000",
+  },
+  "mypets-green": {
+    100: "#34A853",
+    200: "#34A853",
+    300: "#34A853",
+    400: "#34A853",
+    500: "#34A853",
+    600: "#34A853",
+    700: "#34A853",
+    800: "#34A853",
+    900: "#34A853",
+  },
+};
 
 const config = {
-    initialColorMode: "light",
-    useSystemColorMode: false,
-  }
+  initialColorMode: "light",
+  useSystemColorMode: false,
+};
 
 const overrides = {
-    ...chakraTheme,
-    fonts,
-    colors,
-    config
-}
+  ...chakraTheme,
+  fonts,
+  colors,
+  config,
+};
 
-const customTheme = extendTheme(overrides)
+const customTheme = extendTheme(overrides);
 
-export default customTheme
+export default customTheme;
