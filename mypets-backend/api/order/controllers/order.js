63180dab13960c312e8b18e6d911d833fd830e9f
aidanaden <aidanaden@hotmail.com>
2022-03-06 19:13:52 +0800

    Fix all prod backend merge conflicts

--- mypets-backend/api/order/controllers/order.js
+++ mypets-backend/api/order/controllers/order.js
@@ -1,19 +1,19 @@
-+'use strict';
-const { sanitizeEntity } = require('strapi-utils');
-const { customAlphabet } = require('nanoid');
-const finder = require('strapi-utils/lib/finder');
-const stripe = require('stripe')(process.env.STRIPE_SK);
-const nanoid = customAlphabet('0123456789ABCDEF', 10);
-const stripe_free_shipping_id = 'shr_1Js6BxJNGU0rJw40fwzNOwqw'
-const stripe_paid_shipping_id = 'shr_1KRH5iJNGU0rJw40Crb4vtSc'
-const stripe_tax_id = 'txr_1JcuChJNGU0rJw40EKv3YAEt'
-const order_minimum_free_shipping = 39.90
++"use strict";
+const { sanitizeEntity } = require("strapi-utils");
+const { customAlphabet } = require("nanoid");
+const finder = require("strapi-utils/lib/finder");
+const stripe = require("stripe")(process.env.STRIPE_SK);
+const nanoid = customAlphabet("0123456789ABCDEF", 10);
+const stripe_free_shipping_id = "shr_1Js6BxJNGU0rJw40fwzNOwqw";
+const stripe_paid_shipping_id = "shr_1KRH5iJNGU0rJw40Crb4vtSc";
+const stripe_tax_id = "txr_1JcuChJNGU0rJw40EKv3YAEt";
+const order_minimum_free_shipping = 39.9;
 
 /**
  * Return dollar amount in cents
  * @param {number} number
  */
-const fromDecimalToInt = (number) => parseInt(number * 100)
+const fromDecimalToInt = (number) => parseInt(number * 100);
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
@@ -21,221 +21,202 @@ const fromDecimalToInt = (number) => parseInt(number * 100)
  */
 
 module.exports = {
+  /**
+   * Only returns orders that belongs to the logged in user
+   * @param {any} ctx
+   */
+  async find(ctx) {
+    const { user } = ctx.state; // get user
 
-    /**
-     * Only returns orders that belongs to the logged in user
-     * @param {any} ctx
-     */
-    async find(ctx) {
-        const { user } = ctx.state // get user
-
-        let entities
-        if (ctx.query._q) {
-            entities = await strapi.services.order.search({...ctx.query, user: user.id})
-        } else {
-            entities = await strapi.services.order.find({...ctx.query, user: user.id})
-        }
-
-        for (let i=0; i < entities.length; i++) {
-
-            let entity = entities[i]
-            for (let j=0; j < entity.order_products.length; j++) {
-                entity.order_products[j].variant = await strapi.services.variant.findOne({ id: entity.order_products[j].variant })
-                // console.log('order object product value updated to: ', entity.order_products[j].variant)
-            }
-        }
-
-        return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.order }))
-    },
-
-    /**
-     * Returns one order, as long as it belongs to the logged in user
-     * @param {any} ctx
-     */
-    async findOne(ctx) {
-        const { id } = ctx.params
-        const { user } = ctx.state
-
-        const entity = await strapi.services.order.findOne({ id, user: user.id })
-
-        for (let i=0; i < entity.order_products.length; i++) {
-            entity.order_products[i].variant = await strapi.services.variant.findOne({ id: entity.order_products[i].variant })
-        }
-
-        return sanitizeEntity(entity, { model: strapi.models.order })
-    },
-
-    /**
-     * Creates an order & sets up the stripe checkout session for frontend
-     * @param {any} ctx
-     */
-
-    async create(ctx) {
-        const cart = ctx.request.body
-        const { user } = ctx.state
-<<<<<<< HEAD
-=======
-        const profile = await strapi.services.profile.findOne({ user: user.id })
-        const BASE_URL = process.env.FRONTEND_URL || 'https://mypets.sg'
-        var userShippingAddress
-
-        console.log('base url: ', BASE_URL)
->>>>>>> 63e2ffe9d079c0b4a1416b59c5268378f5a6999d
-
-        if (!cart) {
-            return ctx.throw(400, 'Please specify a cart.')
-        }
-
-        const order_items = cart.order_products.map((orderProduct,i) => {
-            var orderProductUnitPrice = 0;
-            if orderProduct.variant.discounted_price && orderProduct.variant.discounted_price > 0 {
-                orderProductUnitPrice = fromDecimalToInt(orderProduct.variant.discounted_price)
-            } else {
-                orderProductUnitPrice = fromDecimalToInt(orderProduct.variant.price)
-            const data = {
-                price_data: {
-                    currency: 'sgd',
-                    product_data: {
-                        name: orderProduct.variant.product.name,
-                        images: [orderProduct.variant.product.image.url],
-                    },
-                    unit_amount: orderProductUnitPrice
-                },
-                quantity: orderProduct.quantity,
-                //tax_rates: [stripe_tax_id],
-            }
-
-            return data
-        })
-
-        if (profile.address && profile.postal && profile.location) {
-            userShippingAddress = {
-                line1: profile.address,
-                city: 'Singapore',
-                country: 'SG',
-                line2: profile.unit,
-                postal_code: profile.postal,
-                state: 'Singapore',
-            }
-        }
-
-        //const userCustomerShipping = {
-        //    address: userShippingAddress ? userShippingAddress : "",
-        //    name: profile.username ? profile.username : ""
-        //}
-
-        //const userCustomer = await stripe.customers.create({
-        //    description: profile.username ? profile.username : "",
-        //    email: user.email,
-        //    address: userShippingAddress ? userShippingAddress : null,
-        //    shipping: userShippingAddress ? userCustomerShipping : null,
-        //})
-
-        //console.log('created user customer object: ', userCustomer)
-
-        const session = await stripe.checkout.sessions.create({
-            payment_method_types: ['card'],
-            customer_email: user.email,
-            //customer: userCustomer.id,
-            mode: 'payment',
-<<<<<<< HEAD
-            success_url: `${FRONTEND_URL}/success?session_id={CHECKOUT_SESSION_ID}`,
-            cancel_url: FRONTEND_URL,
-            line_items: order_items,
-            allow_promotion_codes: true,
-        })
-=======
-            success_url: `${BASE_URL}/success?session_id={CHECKOUT_SESSION_ID}`,
-            cancel_url: BASE_URL,
-            line_items: order_items,
-            allow_promotion_codes: true,
-            shipping_rates: cart.total_price >= order_minimum_free_shipping ? [stripe_free_shipping_id] : [stripe_paid_shipping_id],
-            shipping_address_collection: {
-                allowed_countries: ['SG'],
-            }
-        })
-
->>>>>>> 63e2ffe9d079c0b4a1416b59c5268378f5a6999d
-        return { id: session.id }
-    },
-
-    /**
-     * Given a checkout session, verifies payment and create the order
-     * @param {any} ctx 
-     */
-    async confirm(ctx) {
-        const { user } = ctx.state
-        const { checkout_session, cart } = ctx.request.body
-        const session = await stripe.checkout.sessions.retrieve(checkout_session)
-<<<<<<< HEAD
-        const cart = await strapi.services.cart.findOne({ user: user.id })
-
-        if (session.payment_status === 'paid') {
-            var orderDate = new Date()
-            const deliveryDays = 6
-            orderDate.setDate(orderDate.getDate() + deliveryDays)
-
-            const sessionTotalPrice = session.amount_total / 100
-            const contributionAmount = sessionTotalPrice * 0.05
-
-            // create order
-            const newOrder = await strapi.services.order.create({
-                user: user.id,
-                order_products: cart.order_products,
-                total_price: sessionTotalPrice,
-                contribution: contributionAmount,
-                status: 'processing',
-                checkout_session: session.id,
-                order_date: new Date(),
-                delivery_date: orderDate
-            })
-
-            // delete current cart
-            const cartId = cart.id
-            const entity = await strapi.services.cart.delete({ id: cartId });
-=======
-        const userCart = await strapi.services.cart.findOne({ user: user.id })
-
-        if (session.payment_status === 'paid') {
-	    var orderDate = new Date()
-            const orderDateDay = orderDate.getDay()
-	    const deliveryDays = 3
-	    orderDate.setDate(orderDate.getDate() + deliveryDays)
-
-            const sessionDiscountValue = session.total_details.amount_discount / 100
-            const sessionShippingValue = session.total_details.amount_shipping / 100
-            //const sessionTaxValue = session.total_details.amount_tax / 100
-            const sessionTotal = session.amount_total / 100
-            const contributionAmount = (sessionTotal - sessionShippingValue) * 0.05
-
-	    // create order
-	    const newOrder = await strapi.services.order.create({
-                order_id: nanoid(),
-	        user: user.id,
-	        order_products: userCart.order_products,
-	        total_price: userCart.total_price,
-                final_price: sessionTotal,
-                contribution_amount: contributionAmount,
-                discount_value: sessionDiscountValue,
-                shipping_fee: sessionShippingValue,
-                //tax_fee: sessionTaxValue,
-                tax_fee: 0,
-	        status: 'processing',
-	        checkout_session: session.id,
-	        order_date: new Date(),
-	        delivery_date: orderDate
-	    })
-
-            console.log('CREATING PAID ORDER: ', newOrder)
-
-            // delete current cart
-            const entity = await strapi.services.cart.delete({ id: userCart.id });
->>>>>>> 63e2ffe9d079c0b4a1416b59c5268378f5a6999d
-            console.log('DELETED CART: ', entity)
-
-            return sanitizeEntity(newOrder, { model: strapi.models.order })
-        } else {
-            ctx.throw(400, "Payment failed. Please contact support.")
-        }
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services.order.search({
+        ...ctx.query,
+        user: user.id,
+      });
+    } else {
+      entities = await strapi.services.order.find({
+        ...ctx.query,
+        user: user.id,
+      });
     }
+
+    for (let i = 0; i < entities.length; i++) {
+      let entity = entities[i];
+      for (let j = 0; j < entity.order_products.length; j++) {
+        entity.order_products[j].variant =
+          await strapi.services.variant.findOne({
+            id: entity.order_products[j].variant,
+          });
+        // console.log('order object product value updated to: ', entity.order_products[j].variant)
+      }
+    }
+
+    return entities.map((entity) =>
+      sanitizeEntity(entity, { model: strapi.models.order })
+    );
+  },
+
+  /**
+   * Returns one order, as long as it belongs to the logged in user
+   * @param {any} ctx
+   */
+  async findOne(ctx) {
+    const { id } = ctx.params;
+    const { user } = ctx.state;
+
+    const entity = await strapi.services.order.findOne({ id, user: user.id });
+
+    for (let i = 0; i < entity.order_products.length; i++) {
+      entity.order_products[i].variant = await strapi.services.variant.findOne({
+        id: entity.order_products[i].variant,
+      });
+    }
+
+    return sanitizeEntity(entity, { model: strapi.models.order });
+  },
+
+  /**
+   * Creates an order & sets up the stripe checkout session for frontend
+   * @param {any} ctx
+   */
+
+  async create(ctx) {
+    const cart = ctx.request.body;
+    const { user } = ctx.state;
+    const profile = await strapi.services.profile.findOne({ user: user.id });
+    const BASE_URL = process.env.FRONTEND_URL || "https://mypets.sg";
+    var userShippingAddress;
+
+    console.log("base url: ", BASE_URL);
+
+    if (!cart) {
+      return ctx.throw(400, "Please specify a cart.");
+    }
+
+    const order_items = cart.order_products.map((orderProduct, i) => {
+      var orderProductUnitPrice = 0;
+      if (
+        orderProduct.variant.discounted_price &&
+        orderProduct.variant.discounted_price > 0
+      ) {
+        orderProductUnitPrice = fromDecimalToInt(
+          orderProduct.variant.discounted_price
+        );
+      } else {
+        orderProductUnitPrice = fromDecimalToInt(orderProduct.variant.price);
+      }
+      const data = {
+        price_data: {
+          currency: "sgd",
+          product_data: {
+            name: orderProduct.variant.product.name,
+            images: [orderProduct.variant.product.image.url],
+          },
+          unit_amount: orderProductUnitPrice,
+        },
+        quantity: orderProduct.quantity,
+        //tax_rates: [stripe_tax_id],
+      };
+
+      return data;
+    });
+
+    if (profile.address && profile.postal && profile.location) {
+      userShippingAddress = {
+        line1: profile.address,
+        city: "Singapore",
+        country: "SG",
+        line2: profile.unit,
+        postal_code: profile.postal,
+        state: "Singapore",
+      };
+    }
+
+    //const userCustomerShipping = {
+    //    address: userShippingAddress ? userShippingAddress : "",
+    //    name: profile.username ? profile.username : ""
+    //}
+
+    //const userCustomer = await stripe.customers.create({
+    //    description: profile.username ? profile.username : "",
+    //    email: user.email,
+    //    address: userShippingAddress ? userShippingAddress : null,
+    //    shipping: userShippingAddress ? userCustomerShipping : null,
+    //})
+
+    //console.log('created user customer object: ', userCustomer)
+
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      customer_email: user.email,
+      //customer: userCustomer.id,
+      mode: "payment",
+      success_url: `${BASE_URL}/success?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: BASE_URL,
+      line_items: order_items,
+      allow_promotion_codes: true,
+      shipping_rates:
+        cart.total_price >= order_minimum_free_shipping
+          ? [stripe_free_shipping_id]
+          : [stripe_paid_shipping_id],
+      shipping_address_collection: {
+        allowed_countries: ["SG"],
+      },
+    });
+
+    return { id: session.id };
+  },
+
+  /**
+   * Given a checkout session, verifies payment and create the order
+   * @param {any} ctx
+   */
+  async confirm(ctx) {
+    const { user } = ctx.state;
+    const { checkout_session, cart } = ctx.request.body;
+    const session = await stripe.checkout.sessions.retrieve(checkout_session);
+    const userCart = await strapi.services.cart.findOne({ user: user.id });
+
+    if (session.payment_status === "paid") {
+      var orderDate = new Date();
+      const orderDateDay = orderDate.getDay();
+      const deliveryDays = 3;
+      orderDate.setDate(orderDate.getDate() + deliveryDays);
+
+      const sessionDiscountValue = session.total_details.amount_discount / 100;
+      const sessionShippingValue = session.total_details.amount_shipping / 100;
+      //const sessionTaxValue = session.total_details.amount_tax / 100
+      const sessionTotal = session.amount_total / 100;
+      const contributionAmount = (sessionTotal - sessionShippingValue) * 0.05;
+
+      // create order
+      const newOrder = await strapi.services.order.create({
+        order_id: nanoid(),
+        user: user.id,
+        order_products: userCart.order_products,
+        total_price: userCart.total_price,
+        final_price: sessionTotal,
+        contribution_amount: contributionAmount,
+        discount_value: sessionDiscountValue,
+        shipping_fee: sessionShippingValue,
+        //tax_fee: sessionTaxValue,
+        tax_fee: 0,
+        status: "processing",
+        checkout_session: session.id,
+        order_date: new Date(),
+        delivery_date: orderDate,
+      });
+
+      console.log("CREATING PAID ORDER: ", newOrder);
+
+      // delete current cart
+      const entity = await strapi.services.cart.delete({ id: userCart.id });
+      console.log("DELETED CART: ", entity);
+
+      return sanitizeEntity(newOrder, { model: strapi.models.order });
+    } else {
+      ctx.throw(400, "Payment failed. Please contact support.");
+    }
+  },
 };
--- mypets-backend/api/profile/controllers/profile.js
+++ mypets-backend/api/profile/controllers/profile.js
@@ -1,5 +1,5 @@
-'use strict';
-const { sanitizeEntity } = require('strapi-utils')
+"use strict";
+const { sanitizeEntity } = require("strapi-utils");
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
@@ -7,68 +7,72 @@ const { sanitizeEntity } = require('strapi-utils')
 // const { parseMultipartData, sanitizeEntity } = require('strapi-utils')
 
 module.exports = {
+  /**
+   * Only returns cart that belongs to the logged in user
+   * @param {any} ctx
+   */
+  async find(ctx) {
+    const { user } = ctx.state;
 
-    /**
-     * Only returns cart that belongs to the logged in user
-     * @param {any} ctx 
-     */
-    async find(ctx) {
-
-        const { user } = ctx.state
-
-        let entities
-        if (ctx.query._q) {
-<<<<<<< HEAD
-            entities = await strapi.services.profile.search({...ctx.query, user: user.id })
-        } else {
-            entities = await strapi.services.profile.find({...ctx.query, user: user.id })
-=======
-            entities = await strapi.services.profile.search({...ctx.query, user: user.id})
-        } else {
-            entities = await strapi.services.profile.find({...ctx.query, user: user.id})
->>>>>>> 63e2ffe9d079c0b4a1416b59c5268378f5a6999d
-        }
-
-        return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.profile }))
-    },
-    
-    /**
-     * Returns one cart, as long as it belongs to the logged in user
-     * @param {any} ctx 
-     */
-    async findOne(ctx) {
-        const { id } = ctx.params
-        const { user } = ctx.state
-
-        const entity = await strapi.services.profile.findOne({ id, user: user.id })
-
-        return sanitizeEntity(entity, { model: strapi.models.profile })
-    },
-
-    /**
-     * Create profile
-     * @param {any} ctx 
-     */
-    async create(ctx) {
-
-        const { user } = ctx.state
-        // console.log('creating profile with user id: ', user.id)
-        const entity = await strapi.services.profile.create({...ctx.request.body, user: user.id })
-
-        return sanitizeEntity(entity, { model: strapi.models.profile })
-    },
-
-    /**
-     * Update profile
-     * @param {any} ctx 
-     */
-    async update(ctx) {
-
-        const { id } = ctx.params
-        const { user } = ctx.state
-        // console.log('updating profile with user id: ', user.id)
-        const entity = await strapi.services.profile.update({ id }, {...ctx.request.body, user: user.id })
-
-        return sanitizeEntity(entity, { model: strapi.models.profile })
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services.profile.search({
+        ...ctx.query,
+        user: user.id,
+      });
+    } else {
+      entities = await strapi.services.profile.find({
+        ...ctx.query,
+        user: user.id,
+      });
     }
+
+    return entities.map((entity) =>
+      sanitizeEntity(entity, { model: strapi.models.profile })
+    );
+  },
+
+  /**
+   * Returns one cart, as long as it belongs to the logged in user
+   * @param {any} ctx
+   */
+  async findOne(ctx) {
+    const { id } = ctx.params;
+    const { user } = ctx.state;
+
+    const entity = await strapi.services.profile.findOne({ id, user: user.id });
+
+    return sanitizeEntity(entity, { model: strapi.models.profile });
+  },
+
+  /**
+   * Create profile
+   * @param {any} ctx
+   */
+  async create(ctx) {
+    const { user } = ctx.state;
+    // console.log('creating profile with user id: ', user.id)
+    const entity = await strapi.services.profile.create({
+      ...ctx.request.body,
+      user: user.id,
+    });
+
+    return sanitizeEntity(entity, { model: strapi.models.profile });
+  },
+
+  /**
+   * Update profile
+   * @param {any} ctx
+   */
+  async update(ctx) {
+    const { id } = ctx.params;
+    const { user } = ctx.state;
+    // console.log('updating profile with user id: ', user.id)
+    const entity = await strapi.services.profile.update(
+      { id },
+      { ...ctx.request.body, user: user.id }
+    );
+
+    return sanitizeEntity(entity, { model: strapi.models.profile });
+  },
 };
