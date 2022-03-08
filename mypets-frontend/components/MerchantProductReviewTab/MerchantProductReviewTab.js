930d714622cee7bf58d142a6fe46d65f416f6e47
aidanaden <aidanaden@hotmail.com>
2022-03-08 20:35:54 +0800

    Remove tab from merchant page

--- mypets-frontend/components/MerchantProductReviewTab/MerchantProductReviewTab.js
+++ mypets-frontend/components/MerchantProductReviewTab/MerchantProductReviewTab.js
@@ -37,72 +37,73 @@ function MerchantProductReviewTab({
   // })
 
   return (
-    <Tabs variant="unstyled" size="md" defaultIndex={0} mt={8} p={0}>
-      <TabList justifyContent="center">
-        <Tab
-          rounded="full"
-          _active={{
-            color: "white",
-            bgGradient: "linear(to-t, mypets.900, mypets.100)",
-          }}
-          _selected={{
-            color: "white",
-            bgGradient: "linear(to-t, mypets.900, mypets.100)",
-          }}
-          mr={4}
+    <Flex justifyContent="space-between" direction="row" mt={8}>
+      <Stack w="100%" direction="column" spacing={{ base: 8, lg: 12 }}>
+        <Stack
+          direction="row"
+          align="stretch"
+          justify="space-between"
+          display={{ base: "none", lg: "inherit" }}
+          // bg='red.100'
         >
-          Products
-        </Tab>
-        {/* <Tab
-          rounded="full"
-          _active={{
-            color: "white",
-            bgGradient: "linear(to-t, mypets.900, mypets.100)",
-          }}
-          _selected={{
-            color: "white",
-            bgGradient: "linear(to-t, mypets.900, mypets.100)",
-          }}
-        >
-          Reviews
-        </Tab> */}
-      </TabList>
-      <TabPanels mt={4} px={0}>
-        <TabPanel px={0}>
-          <Flex justifyContent="space-between" direction="row">
-            <Stack w="100%" direction="column" spacing={{ base: 8, lg: 12 }}>
-              <Stack
-                direction="row"
-                align="stretch"
-                justify="space-between"
-                display={{ base: "none", lg: "inherit" }}
-                // bg='red.100'
-              >
-                <Box>
-                  <SectionHeader>Animal</SectionHeader>
-                  <AnimalList
-                    animals={getAnimals(merchantProducts)}
-                    setSelectedAnimal={setSelectedAnimal}
-                  />
-                </Box>
-                <Spacer />
-                <SortMenu setSortMethod={setSortMethod} />
-              </Stack>
-              <ProductSectionList
-                products={merchantProducts}
-                categories={categories}
-                sortMethod={sortMethod}
-                setSortMethod={setSortMethod}
-                selectedAnimal={selectedAnimal}
-              />
-            </Stack>
-          </Flex>
-        </TabPanel>
-        {/* <TabPanel>
-          <MerchantReviewsList reviews={merchantReviews} />
-        </TabPanel> */}
-      </TabPanels>
-    </Tabs>
+          <Box>
+            <SectionHeader>Animal</SectionHeader>
+            <AnimalList
+              animals={getAnimals(merchantProducts)}
+              setSelectedAnimal={setSelectedAnimal}
+            />
+          </Box>
+          <Spacer />
+          <SortMenu setSortMethod={setSortMethod} />
+        </Stack>
+        <ProductSectionList
+          products={merchantProducts}
+          categories={categories}
+          sortMethod={sortMethod}
+          setSortMethod={setSortMethod}
+          selectedAnimal={selectedAnimal}
+        />
+      </Stack>
+    </Flex>
+    // <Tabs variant="unstyled" size="md" defaultIndex={0} p={0}>
+    //   <TabList justifyContent="center">
+    //     <Tab
+    //       rounded="full"
+    //       _active={{
+    //         color: "white",
+    //         bgGradient: "linear(to-t, mypets.900, mypets.100)",
+    //       }}
+    //       _selected={{
+    //         color: "white",
+    //         bgGradient: "linear(to-t, mypets.900, mypets.100)",
+    //       }}
+    //       mr={4}
+    //     >
+    //       Products
+    //     </Tab>
+    //     {/* <Tab
+    //       rounded="full"
+    //       _active={{
+    //         color: "white",
+    //         bgGradient: "linear(to-t, mypets.900, mypets.100)",
+    //       }}
+    //       _selected={{
+    //         color: "white",
+    //         bgGradient: "linear(to-t, mypets.900, mypets.100)",
+    //       }}
+    //     >
+    //       Reviews
+    //     </Tab> */}
+    //   </TabList>
+    //   <TabPanels mt={4} px={0}>
+    //     <TabPanel px={0}>
+
+    //     </TabPanel>
+    //     {/* <TabPanel>
+    //       <MerchantReviewsList reviews={merchantReviews} />
+    //     </TabPanel> */}
+    //   </TabPanels>
+    // </Tabs>
   );
 }
 
