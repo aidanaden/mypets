8b72d2599eb3294b982fd188af5958c6f741ce53
aidanaden <aidanaden@hotmail.com>
2022-04-05 23:46:48 +0800

    BenefitsSection: test benefits data shape

--- mypets-frontend/components/Home/BenefitsSection/index.js
+++ mypets-frontend/components/Home/BenefitsSection/index.js
@@ -2,11 +2,12 @@ import { Box, SimpleGrid } from "@chakra-ui/react";
 import SectionHeader from "../../Layouts/SectionHeader/SectionHeader";
 
 function index({ benefits, spacing = 4, ...props }) {
+  console.log(benefits);
   return (
     <Box {...props}>
       <SectionHeader>Choose Mypets</SectionHeader>
       <SimpleGrid columns={5} spacing={spacing}>
-        {benefits.map((benefit, i) => (
+        {benefits.benefit.map((benefit, i) => (
           <BenefitCard key={`benefit_${i}`} benefit={benefit} />
         ))}
       </SimpleGrid>
