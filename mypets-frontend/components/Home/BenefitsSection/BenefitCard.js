b0660a8b16132c73705c4e915cc0b0c91e295365
aidanaden <aidanaden@hotmail.com>
2022-04-08 05:25:33 +0800

    BenefitsSection: update benefitCard circles

--- mypets-frontend/components/Home/BenefitsSection/BenefitCard.js
+++ mypets-frontend/components/Home/BenefitsSection/BenefitCard.js
@@ -11,7 +11,13 @@ export default function BenefitCard({ benefit, ...props }) {
       p={{ base: 4, md: 8 }}
       {...props}
     >
-      <Circle boxSize={"36"} shadow="md" bg="blue.200" />
+      <Circle
+        boxSize={{ base: "24", md: "32" }}
+        shadow="md"
+        bg="blue.200"
+        borderWidth={"2px"}
+        borderColor="white"
+      />
       <Box bg="red.300" py="auto">
         <Box my="auto" justifySelf={"center"} bg="blue.100">
           <Text mb={2} fontSize="2xl" fontWeight="bold">
--- mypets-frontend/components/Layouts/TopContainer/TopContainer.js
+++ mypets-frontend/components/Layouts/TopContainer/TopContainer.js
@@ -2,12 +2,7 @@ import PageContainer from "../PageContainer/PageContainer";
 
 export default function TopContainer({ children, ...props }) {
   return (
-    <PageContainer
-      pt={{ base: 4, lg: 12 }}
-      pb={{ base: 4, lg: 12 }}
-      bg="blue.100"
-      {...props}
-    >
+    <PageContainer pt={{ base: 4, lg: 12 }} pb={{ base: 4, lg: 12 }} {...props}>
       {children}
     </PageContainer>
   );
