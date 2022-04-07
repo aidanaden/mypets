7d1ca0105ffdf82da4e74fbc50d7b7493d627a61
aidanaden <aidanaden@hotmail.com>
2022-04-08 02:55:00 +0800

    refactor: create TopContainer, FooterContainer, set PageContainer to merchant and animal lists 21

--- mypets-frontend/components/Home/BenefitsSection/index.js
+++ mypets-frontend/components/Home/BenefitsSection/index.js
@@ -1,19 +1,19 @@
 import { Box, SimpleGrid } from "@chakra-ui/react";
 import SectionHeader from "../../Layouts/SectionHeader/SectionHeader";
 import BenefitCard from "./BenefitCard";
-import FooterContainer from "../../Layouts/FooterContainer/FooterContainer";
+import PageContainer from "../../Layouts/PageContainer/PageContainer";
 
 function index({ benefits, spacing = 4, ...props }) {
   return (
     <Box bg="mypets.400">
-      <FooterContainer {...props}>
+      <PageContainer {...props}>
         <SectionHeader textAlign="center">{benefits.Header}</SectionHeader>
         <SimpleGrid columns={2} spacing={spacing}>
           {benefits.benefit.map((benefit, i) => (
             <BenefitCard key={`benefit_${i}`} benefit={benefit} />
           ))}
         </SimpleGrid>
-      </FooterContainer>
+      </PageContainer>
     </Box>
   );
 }
--- mypets-frontend/components/Home/NewsletterSection/index.js
+++ mypets-frontend/components/Home/NewsletterSection/index.js
@@ -1,12 +1,12 @@
 import { Box } from "@chakra-ui/react";
-import PageContainer from "../../Layouts/PageContainer/PageContainer";
+import FooterContainer from "../../Layouts/FooterContainer/FooterContainer";
 import SectionHeader from "../../Layouts/SectionHeader/SectionHeader";
 
 function index({ data, ...props }) {
   return (
-    <PageContainer pb={{ base: 4, lg: 12 }} {...props}>
+    <FooterContainer bg="white" pb={{ base: 4, lg: 12 }} {...props}>
       <SectionHeader>{data.Header}</SectionHeader>
-    </PageContainer>
+    </FooterContainer>
   );
 }
 
