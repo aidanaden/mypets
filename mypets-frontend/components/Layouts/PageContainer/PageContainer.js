2e128297c11a1ba47716daa9277ca5410a764caf
aidanaden <aidanaden@hotmail.com>
2022-04-08 01:39:33 +0800

    refactor: create TopContainer, FooterContainer, set PageContainer to merchant and animal lists 7

--- mypets-frontend/components/Layouts/PageContainer/PageContainer.js
+++ mypets-frontend/components/Layouts/PageContainer/PageContainer.js
@@ -7,6 +7,7 @@ export default function PageContainer({ isFooter, children, ...props }) {
       px={{ base: 4, xl: 0 }}
       pt={{ base: 4, lg: 12 }}
       bg="red.100"
+      mx="auto"
       // pb={isFooter == true ? { base: 4, lg: 12 } : { base: 512, lg: 96 }}
       {...props}
     >
