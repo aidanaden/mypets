1ac6a31caae87a108c3575019dea78d4d52ae9e9
aidanaden <aidanaden@hotmail.com>
2022-04-19 06:15:48 +0800

    BenefitsSection: add benefit image icons, center benefit text 10

--- mypets-frontend/components/Home/BenefitsSection/index.js
+++ mypets-frontend/components/Home/BenefitsSection/index.js
@@ -5,14 +5,16 @@ import PageContainer from "../../Layouts/PageContainer/PageContainer";
 
 function index({ benefits, spacing = 4, ...props }) {
   return (
-    <PageContainer pb={{ base: 4, lg: 12 }} bg="mypets.400" {...props}>
-      <SectionHeader textAlign="center">{benefits.Header}</SectionHeader>
-      <SimpleGrid columns={{ base: 1, md: 2 }} spacing={spacing}>
-        {benefits.benefit.map((benefit, i) => (
-          <BenefitCard key={`benefit_${i}`} benefit={benefit} />
-        ))}
-      </SimpleGrid>
-    </PageContainer>
+    <Box bg="mypets.400">
+      <PageContainer pb={{ base: 8, lg: 12 }} {...props}>
+        <SectionHeader textAlign="center">{benefits.Header}</SectionHeader>
+        <SimpleGrid columns={{ base: 1, md: 2 }} spacing={spacing}>
+          {benefits.benefit.map((benefit, i) => (
+            <BenefitCard key={`benefit_${i}`} benefit={benefit} />
+          ))}
+        </SimpleGrid>
+      </PageContainer>
+    </Box>
   );
 }
 
