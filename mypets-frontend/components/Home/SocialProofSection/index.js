a64fe1d13e883af55e2ca27ddd1d7d0fd1738812
aidanaden <aidanaden@hotmail.com>
2022-04-20 13:12:13 +0800

    SocialProof: add social proof buttons

--- mypets-frontend/components/Home/SocialProofSection/index.js
+++ mypets-frontend/components/Home/SocialProofSection/index.js
@@ -1,5 +1,34 @@
-import React from "react";
+import { FaInstagram, FaFacebook } from "react-icons/fa";
+
+import PageContainer from "../../Layouts/PageContainer/PageContainer";
+import SectionHeader from "../../Layouts/SectionHeader/SectionHeader";
+import MypetsBtn from "../../Common/MypetsBtn/MypetsBtn";
 
 export default function index() {
-  return <div></div>;
+  return (
+    <PageContainer bg="white" {...props}>
+      <SectionHeader textAlign="center" mb={4}>
+        Follow us on our socials
+      </SectionHeader>
+      <Stack
+        direction={{ base: "column", md: "row" }}
+        spacing={{ base: 4, md: 8 }}
+      >
+        <MypetsBtn
+          as={"a"}
+          btnText="@mypets.sg"
+          rounded="full"
+          w={{ base: "100%", md: "xs" }}
+          leftIcon={<FaInstagram />}
+        />
+        <MypetsBtn
+          as={"a"}
+          btnText="Mypets"
+          rounded="full"
+          w={{ base: "100%", md: "xs" }}
+          leftIcon={<FaFacebook />}
+        />
+      </Stack>
+    </PageContainer>
+  );
 }
