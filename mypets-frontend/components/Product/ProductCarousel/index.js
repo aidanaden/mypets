047711e444a4952e4764c5acc0725be6c9ac9fa2
aidanaden <aidanaden@hotmail.com>
2022-06-06 03:15:20 +0800

    test: test product carousel side margins and remove relative pos 2

--- mypets-frontend/components/Product/ProductCarousel/index.js
+++ mypets-frontend/components/Product/ProductCarousel/index.js
@@ -44,12 +44,12 @@ export default function ProductCarousel({ products }) {
       spacing={10}
       alignItems="center"
       justifyContent="space-between"
-      mx={-10}
+      mx={"-40"}
     >
       <Text {...arrowStyles} onClick={prevSlide}>
         &#10094;
       </Text>
-      <Flex w="full" overflow="hidden">
+      <Flex w="full" overflow="hidden" pos="relative">
         <HStack
           // columns={{ base: 2, md: 4, lg: 4, xl: 5 }}
           spacing={{ base: 4 }}
@@ -70,7 +70,7 @@ export default function ProductCarousel({ products }) {
             // </Box>
           ))}
         </HStack>
-        <HStack justify="center" w="full">
+        <HStack justify="center" pos="absolute" bottom="0" w="full">
           {Array.from({ length: slidesCount }).map((_, slide) => (
             <Box
               key={`dots-${slide}`}
