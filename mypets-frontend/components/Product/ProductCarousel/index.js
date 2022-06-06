36fef06886c48bbb9eec1c7083e5e93d8dc8717e
aidanaden <aidanaden@hotmail.com>
2022-06-06 19:31:27 +0800

    chore: update productListCard size in carousel 5

--- mypets-frontend/components/Product/ProductCarousel/index.js
+++ mypets-frontend/components/Product/ProductCarousel/index.js
@@ -68,7 +68,7 @@ export default function ProductCarousel({ products, ...props }) {
                 <ProductListCard
                   product={product}
                   key={`slide-${sid}`}
-                  w={{ lg: "195px" }}
+                  w={{ lg: "210px" }}
                 />
               ))}
             </HStack>
