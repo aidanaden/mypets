fa93da71776469b0669496892bbf25410d0cfd58
aidanaden <aidanaden@hotmail.com>
2022-06-06 20:36:39 +0800

    chore: update productListCard size in carousel minW 190

--- mypets-frontend/components/Product/ProductCarousel/index.js
+++ mypets-frontend/components/Product/ProductCarousel/index.js
@@ -68,7 +68,7 @@ export default function ProductCarousel({ products, ...props }) {
                 <ProductListCard
                   product={product}
                   key={`slide-${sid}`}
-                  minW={{ lg: "184px" }}
+                  minW={{ lg: "190px" }}
                 />
               ))}
             </HStack>
