b97c4692e4719e950f24eab6e9ada57af8e3bba1
aidanaden <aidanaden@hotmail.com>
2022-06-20 02:21:20 +0800

    chore: add import

--- mypets-backend/api/section/controllers/section.js
+++ mypets-backend/api/section/controllers/section.js
@@ -1,4 +1,5 @@
 "use strict";
+const { sanitizeEntity } = require("strapi-utils");
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
