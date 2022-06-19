8599c67734fdf7a34c536101aab7b722083179e5
aidanaden <aidanaden@hotmail.com>
2022-06-20 02:24:26 +0800

    chore: add import to section

--- mypets-backend/api/section/controllers/section.js
+++ mypets-backend/api/section/controllers/section.js
@@ -1,8 +1,36 @@
-'use strict';
+"use strict";
+const { sanitizeEntity } = require("strapi-utils");
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  /**
+   * Returns sections
+   * @param {any} ctx
+   */
+  async find(ctx) {
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services.section.search({ ...ctx.query });
+    } else {
+      entities = await strapi.services.section.find({ ...ctx.query });
+    }
+    return entities.map((entity) =>
+      sanitizeEntity(entity, { model: strapi.models.section })
+    );
+  },
+
+  /**
+   * Returns section with matching section id
+   * @param {any} ctx
+   */
+  async findOne(ctx) {
+    const { id } = ctx.params;
+
+    let entity = await strapi.services.section.findOne({ id: id });
+    return sanitizeEntity(entity, { model: strapi.models.section });
+  },
+};
